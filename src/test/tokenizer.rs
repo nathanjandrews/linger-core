fd00f99a0e618b533832cf91db917e7ce60360e9
Nathan Andrews <nandrews.programming@gmail.com>
2022-12-26 06:54:02 -0500

    fixed broken tests due to Token definition change

--- src/test/tokenizer.rs
+++ src/test/tokenizer.rs
@@ -23,28 +23,19 @@ fn tokenize_error(s: &str) -> bool {
 
 #[test]
 fn numbers() {
-    assert!(tokenize_success("1234", vec![Token::NUM { n: 1234 }]));
-    assert!(tokenize_success("-1234", vec![Token::NUM { n: -1234 }]));
+    assert!(tokenize_success("1234", vec![Token::NUM(1234)]));
+    assert!(tokenize_success("-1234", vec![Token::NUM(-1234)]));
 }
 
 #[test]
 fn ids() {
-    assert!(tokenize_success("abc", vec![Token::ID { value: "abc" }]));
-    assert!(tokenize_success("AbC", vec![Token::ID { value: "AbC" }]));
-    assert!(tokenize_success("aBc_", vec![Token::ID { value: "aBc_" }]));
-    assert!(tokenize_success(
-        "abc_12",
-        vec![Token::ID { value: "abc_12" }]
-    ));
-    assert!(tokenize_success(
-        "abc_12_DE",
-        vec![Token::ID { value: "abc_12_DE" }]
-    ));
+    assert!(tokenize_success("abc", vec![Token::ID("abc")]));
+    assert!(tokenize_success("AbC", vec![Token::ID("AbC")]));
+    assert!(tokenize_success("aBc_", vec![Token::ID("aBc_")]));
+    assert!(tokenize_success("abc_12", vec![Token::ID("abc_12")]));
+    assert!(tokenize_success("abc_12_DE", vec![Token::ID("abc_12_DE")]));
 
-    assert!(tokenize_mismatch(
-        "12345",
-        vec![Token::ID { value: "12345" }]
-    ));
+    assert!(tokenize_mismatch("12345", vec![Token::ID("12345")]));
 
     assert!(tokenize_error("1a"));
     assert!(tokenize_error("_abc"));
