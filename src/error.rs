e505a6f15ced50221e32135f4b76f9d73feca20a
Nathan Andrews <nandrews.programming@gmail.com>
2022-12-26 21:29:05 -0500

    added helper function for error messaging on ParseErrors

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-use crate::tokenizer::Token;
+use crate::tokenizer::{Token, Tokens};
 
 #[derive(Debug, Clone)]
 pub struct TokenizerError;
@@ -21,7 +21,14 @@ pub enum LingerError<'a> {
 impl fmt::Display for LingerError<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            LingerError::ParseError(_) => write!(f, "parse error"),
+            LingerError::ParseError(err) => {
+                match err {
+                    ParseError::NoMain => write!(f, "main procedure not found"),
+                    ParseError::MultipleMain => write!(f, "multiple main procedures found"),
+                    ParseError::MissingSemicolon => write!(f, "missing semicolon"),
+                    ParseError::UnexpectedToken(tokens) => write!(f, "unexpected tokens: {}", Tokens(tokens.to_vec())),
+                }
+            }
             LingerError::TokenizerError(_) => write!(f, "tokenizer error"),
         }
     }
