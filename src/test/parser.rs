f3d4a211fe5bffcc24e7fab9c24bc822bf1cc661
Nathan Andrews <nandrews.programming@gmail.com>
2022-12-27 22:25:34 -0500

    added program integration tests

--- src/test/parser.rs
+++ src/test/parser.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use crate::{
-    parser::{parse_program, Expr::*, Program, Statement},
+    parser::{parse_program, Expr::*, Program, Statement, Builtin::*},
     tokenizer::{
         Token,
         TokenValue::{self, *},
@@ -10,9 +10,9 @@ use crate::{
 #[allow(dead_code)]
 fn tokens_from_values(values: Vec<TokenValue>) -> Vec<Token> {
     values
-    .into_iter()
-    .map(|value| Token(value, 0, 0))
-    .collect::<Vec<Token>>()
+        .into_iter()
+        .map(|value| Token(value, 0, 0))
+        .collect::<Vec<Token>>()
 }
 
 #[allow(dead_code)]
@@ -32,7 +32,6 @@ fn parse_mismatch(values: Vec<TokenValue>, expected: Program) -> bool {
         Err(_) => false,
     }
 }
-
 
 #[allow(dead_code)]
 fn parse_error(values: Vec<TokenValue>) -> bool {
@@ -61,7 +60,7 @@ fn print_ten() {
         ],
         Program {
             procedures: vec![],
-            main: vec![Statement::Expr(Call("print", vec![Num(10)]))]
+            main: vec![Statement::Expr(PrimitiveCall(Print, vec![Num(10)]))]
         }
     ))
 }
---
+++ tests/integration_tests.rs
@@ -0,0 +1,56 @@
+use std::process::Command;
+
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+
+fn file_name_to_path(s: &str) -> String {
+  return format!("tests/examples/{}.ling", s);
+}
+
+#[test]
+fn early_return() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("linger")?;
+
+    cmd.arg(file_name_to_path("early_return"));
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("15"));
+
+    Ok(())
+}
+
+#[test]
+fn fibonacci() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("linger")?;
+
+    cmd.arg(file_name_to_path("fibonacci"));
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("55"));
+
+    Ok(())
+}
+
+#[test]
+fn identity() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("linger")?;
+
+    cmd.arg(file_name_to_path("identity"));
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("5true"));
+
+    Ok(())
+}
+
+#[test]
+fn print_ten() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("linger")?;
+
+    cmd.arg(file_name_to_path("print_ten"));
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("10"));
+
+    Ok(())
+}
