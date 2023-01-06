cbf3c725d36dc3204549d07c98722a6f9d04306b
Nathan Andrews <nandrews.programming@gmail.com>
2023-01-06 18:12:33 -0500

    added scope tests

--- tests/scope.rs
+++ tests/scope.rs
@@ -1,6 +1,7 @@
 use std::process::Command;
 
 use assert_cmd::prelude::*;
+use linger::error::RuntimeError;
 use predicates::prelude::predicate::str::contains;
 
 fn file_name_to_path(s: &str) -> String {
@@ -35,6 +36,36 @@ fn procedure_shadowing() -> TestResult {
 
     cmd.arg(file_name_to_path("procedure_shadowing"));
     cmd.assert().success().stdout(contains("success"));
+
+    Ok(())
+}
+
+#[test]
+fn err_for_loop_var_scope() -> TestResult {
+    let mut cmd = Command::cargo_bin("linger")?;
+
+    cmd.arg(file_name_to_path("err-for_loop_var_scope"));
+    cmd.assert()
+        .failure()
+        .stderr(contains(
+            RuntimeError::UnknownVariable("a".to_string()).to_string(),
+        ))
+        .stdout("");
+
+    Ok(())
+}
+
+#[test]
+fn err_unknown_var() -> TestResult {
+    let mut cmd = Command::cargo_bin("linger")?;
+
+    cmd.arg(file_name_to_path("err-unknown_var"));
+    cmd.assert()
+        .failure()
+        .stderr(contains(
+            RuntimeError::UnknownVariable("a".to_string()).to_string(),
+        ))
+        .stdout("");
 
     Ok(())
 }
