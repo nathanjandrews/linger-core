bf123583d91310cd2cb803f917e05a45c30af00d
Nathan Andrews <nandrews.programming@gmail.com>
2023-01-06 11:34:08 -0500

    added while loop tests

---
+++ tests/loops.rs
@@ -0,0 +1,70 @@
+use std::process::Command;
+
+use assert_cmd::prelude::*;
+use predicates::prelude::predicate::str::contains;
+
+fn file_name_to_path(s: &str) -> String {
+    return format!("test_programs/loops/{}.ling", s);
+}
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+#[test]
+fn while_statement() -> TestResult {
+    let mut cmd = Command::cargo_bin("linger")?;
+
+    cmd.arg(file_name_to_path("while"));
+    cmd.assert().success().stdout(contains("5 4 3 2 1"));
+
+    Ok(())
+}
+
+#[test]
+fn while_with_break() -> TestResult {
+    let mut cmd = Command::cargo_bin("linger")?;
+
+    cmd.arg(file_name_to_path("while_with_break"));
+    cmd.assert().success().stdout(contains("5 4 3"));
+
+    Ok(())
+}
+
+#[test]
+fn while_with_continue() -> TestResult {
+    let mut cmd = Command::cargo_bin("linger")?;
+
+    cmd.arg(file_name_to_path("while_with_continue"));
+    cmd.assert().success().stdout(contains("10 8 6 4 2"));
+
+    Ok(())
+}
+
+#[test]
+fn while_with_break_and_continue() -> TestResult {
+    let mut cmd = Command::cargo_bin("linger")?;
+
+    cmd.arg(file_name_to_path("while_with_break_and_continue"));
+    cmd.assert().success().stdout(contains("20 18 16 14 12 10"));
+
+    Ok(())
+}
+
+#[test]
+fn nested_break() -> TestResult {
+    let mut cmd = Command::cargo_bin("linger")?;
+
+    cmd.arg(file_name_to_path("nested_break"));
+    cmd.assert().success().stdout(contains("3 3 \n2 2 \n1 1"));
+
+    Ok(())
+}
+
+#[test]
+fn nested_continue() -> TestResult {
+    let mut cmd = Command::cargo_bin("linger")?;
+
+    cmd.arg(file_name_to_path("nested_continue"));
+    cmd.assert().success().stdout(contains("4 2 0 \n4 2 0 \n4 2 0 "));
+
+    Ok(())
+}
