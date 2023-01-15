f05127feb362162e6b23cecef0db1b156b23aa15
Nathan Andrews <nandrews.programming@gmail.com>
2023-01-15 18:15:51 -0500

    moved builtins tests to miscellaneous

--- tests/miscellaneous.rs
+++ tests/miscellaneous.rs
@@ -1,7 +1,7 @@
 use std::process::Command;
 
 use assert_cmd::prelude::*;
-use linger::error::{ParseError, TokenizerError};
+use linger::error::{ParseError, TokenizerError, RuntimeError};
 use predicates::prelude::predicate::str::{contains, starts_with};
 
 fn file_name_to_path(s: &str) -> String {
@@ -16,6 +16,47 @@ fn comments() -> TestResult {
 
     cmd.arg(file_name_to_path("comments"));
     cmd.assert().success().stdout(contains("success"));
+
+    Ok(())
+}
+
+#[test]
+fn is_empty() -> TestResult {
+    let mut cmd = Command::cargo_bin("linger")?;
+    cmd.arg(file_name_to_path("is_empty_true"));
+    cmd.assert().success().stdout(starts_with("true"));
+
+    let mut cmd = Command::cargo_bin("linger")?;
+    cmd.arg(file_name_to_path("is_empty_false"));
+    cmd.assert().success().stdout(starts_with("false"));
+
+    Ok(())
+}
+
+#[test]
+fn is_nil() -> TestResult {
+    let mut cmd = Command::cargo_bin("linger")?;
+    cmd.arg(file_name_to_path("is_nil_true"));
+    cmd.assert().success().stdout(starts_with("true"));
+
+    let mut cmd = Command::cargo_bin("linger")?;
+    cmd.arg(file_name_to_path("is_nil_false"));
+    cmd.assert().success().stdout(contains("false").count(4));
+
+    Ok(())
+}
+
+#[test]
+fn err_is_empty_non_list() -> TestResult {
+    let mut cmd = Command::cargo_bin("linger")?;
+
+    cmd.arg(file_name_to_path("err-is_empty_non_list"));
+    cmd.assert()
+        .failure()
+        .stderr(starts_with(
+            RuntimeError::ExpectedList("true".to_string()).to_string(),
+        ))
+        .stdout("");
 
     Ok(())
 }
